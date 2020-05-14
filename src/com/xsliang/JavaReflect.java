fa1506ce1eb9aa5d8502f247e2b22f81fa290fb5
xiao songliang <xsliang17@hotmail.com>
2020-05-14 14:06:03 +0800

    JavaAnnotation

--- src/com/xsliang/JavaReflect.java
+++ src/com/xsliang/JavaReflect.java
@@ -30,6 +30,24 @@ public class JavaReflect {
 	@SuppressWarnings({ "rawtypes", "unchecked" })
 	public static void main(String[] args) {
 		try {
+			System.out.println("Hello,Akina!");
+	        System.out.println("\033[30;4m" + "Hello,Akina!" + "\033[0m");
+	        System.out.println("\033[31;4m" + "Hello,Akina!" + "\033[0m");
+	        System.out.println("\033[32;4m" + "Hello,Akina!" + "\033[0m");
+	        System.out.println("\033[33;4m" + "Hello,Akina!" + "\033[0m");
+	        System.out.println("\033[34;4m" + "Hello,Akina!" + "\033[0m");
+	        System.out.println("\033[35;4m" + "Hello,Akina!" + "\033[0m");
+	        System.out.println("\033[36;4m" + "Hello,Akina!" + "\033[0m");
+	        System.out.println("\033[37;4m" + "Hello,Akina!" + "\033[0m");
+	        System.out.println("\033[40;31;4m" + "Hello,Akina! bg black text red" + "\033[0m");
+	        System.out.println("\033[41;32;4m" + "Hello,Akina! bg red text green" + "\033[0m");
+	        System.out.println("\033[42;33;4m" + "Hello,Akina!" + "\033[0m");
+	        System.out.println("\033[43;34;4m" + "Hello,Akina!" + "\033[0m");
+	        System.out.println("\033[44;35;4m" + "Hello,Akina!" + "\033[0m");
+	        System.out.println("\033[45;36;4m" + "Hello,Akina!" + "\033[0m");
+	        System.out.println("\033[46;37;4m" + "Hello,Akina!" + "\033[0m");
+	        System.out.println("\033[47;4m" + "Hello,Akina!" + "\033[0m");
+			
 			Class reClass = JavaReflect.class;
 			Class reClass2=Class.forName("com.xsliang.JavaReflect");
 			
